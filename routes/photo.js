91b0d8fa149945da8728b90ca747e073a6260dd3
Jgorenburg <jacobgorenburg@gmail.com>
2024-10-08 14:04:03 -0400

    photo welcome page

--- routes/photo.js
+++ routes/photo.js
@@ -1,8 +1,9 @@
 var express = require('express');
-var router = express.Router();
 var path = require('path');
 var fs = require('fs');
 var appRoot = require('app-root-path');
+
+var router = express.Router();
 
 class Image {
     constructor(alt, loc) {
@@ -11,8 +12,24 @@ class Image {
     }
 }
 
-router.get('/:type/:title', function (req, res, next) {
-    console.log(req.params)
+class Album {
+    constructor(folder, image) {
+        this.title = folder
+        this.thumbnail = image
+    }
+}
+
+function makeImage(imagePath, fullPath) {
+    var filename = path.parse(imagePath).name;
+    const index = filename.indexOf('-');
+    if (index > -1) {
+        filename = filename.slice(index + 1);
+    }
+    return new Image(filename.replaceAll(constant.SPACE_EQUIVILANTS, " "), path.join("/images", fullPath, imagePath))
+}
+
+router.get('/:type(occasions|techniques)/:title', function (req, res, next) {
+    // console.log(req.params)
 
     fs.readdir(path.join(appRoot.toString(), '/public/images', req.path), (err, files) => {
         if (err) {
@@ -21,11 +38,13 @@ router.get('/:type/:title', function (req, res, next) {
             return;
         }
         //files.forEach(image => console.log(image))
-        const fimages = files.map(file => new Image(path.parse(file).name, path.join("/images", req.path, file)))
+        const fimages = files.map(file => {
+            return makeImage(file, req.path);
+        });
         //fimages.forEach(image => console.log(image))
         const title = req.params.title;
 
-        res.render('photography/template', {
+        res.render('photography/album', {
             title: title[0].toLocaleUpperCase() + title.substring(1).toLocaleLowerCase(),
             album: fimages
         });
@@ -33,9 +52,39 @@ router.get('/:type/:title', function (req, res, next) {
 });
 
 // TODO: make landing page for photography
-router.get('/*', function (_, res, next) {
-    console.log("salmon")
-    res.render('index', { title: 'Photography' });
+router.get('/*', function (req, res) {
+    var occasions = [];
+    var styles = []
+
+    fs.readdirSync(path.join(appRoot.toString(), '/public/images/occasions')).forEach(album => {
+        const images = fs.readdirSync(path.join(appRoot.toString(), '/public/images/occasions', album));
+        const image = makeImage(images[0], path.join("/occasions", album));
+        occasions.push(new Album(album.replaceAll(constant.SPACE_EQUIVILANTS, " "), image));
+    });
+
+
+    fs.readdirSync(path.join(appRoot.toString(), '/public/images/techniques')).forEach(album => {
+        const images = fs.readdirSync(path.join(appRoot.toString(), '/public/images/techniques', album));
+        const image = makeImage(images[0], path.join("/techniques", album));
+        styles.push(new Album(album.replaceAll(constant.SPACE_EQUIVILANTS, " "), image));
+    });
+
+    // console.log(occasions)
+    // console.log(styles)
+    res.render('photography/gallery', { occasions: occasions, styles: styles });
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
